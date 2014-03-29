4ed7a3d623f5d91f22a7f89dc933af0d88bb82b4
Jonathon Grigg <jonathongrigg@gmail.com>
2014-03-29 22:12:53 +0800

    json based 'api' calls to and from javascript rather than rendering whole page responses

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,10 +1,37 @@
+var getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var getPost = function(latitude, longitude) {
+    requestData = JSON.stringify({
+        "latitude": latitude,
+        "longitude": longitude,
+    });
+    console.log(requestData);
+    $.ajax({
+        url: '/get_post',
+        dataType: 'json',
+        contentType: 'application/json; charset=UTF-8',
+        data: requestData,
+        type: 'POST',
+        success: function(data) {
+            console.log(data);
+            number = getRandomInt(0, data.posts.length - 1);
+            console.log(number);
+            $('#post h1').html(data.posts[number]['title']);
+            $('#post h2').html("By ^" + data.posts[number]['author'] + " at " + data.posts[number]['date_created']);
+            $('#post article').html(data.posts[number]['content']);
+        }
+    })
+};
+
 $(document).ready(function() {
     $('#ip_location').click(function() {
         $.ajax({
             url: 'http://freegeoip.net/json/',
             dataType: 'json',
             success: function(data) {
-                window.location.href = '/near?latitude=' + data.latitude + '&longitude=' + data.longitude;
+                getPost(data.latitude, data.longitude);
             }
         });
     });
@@ -12,10 +39,51 @@ $(document).ready(function() {
     $('#geo_location').click(function() {
         if (Modernizr.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
-                window.location.href = '/near?latitude=' + position.coords.latitude + '&longitude=' + position.coords.longitude;
+                getPost(position.coords.latitude, position.coords.longitude);
             });
         } else {
             // no native support; maybe try a fallback?
+        }
+    });
+
+    $('#add_post').click(function() {
+        var latitude = 0;
+        var longitude = 0;
+        var submit = function() {
+            requestData = JSON.stringify({
+                "title": $('#title').val(),
+                "content": $('#content').val(),
+                "latitude": latitude,
+                "longitude": longitude
+            });
+            console.log(requestData);
+            $.ajax({
+                url: '/add_post',
+                dataType: 'json',
+                contentType: 'application/json; charset=UTF-8',
+                data: requestData,
+                type: 'POST',
+                success: function(data) {
+                    console.log(data);
+                }
+            })
+        }
+        if (Modernizr.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                latitude = position.coords.latitude;
+                longitude = position.coords.longitude;
+                submit();
+            });
+        } else {
+            $.ajax({
+                url: 'http://freegeoip.net/json/',
+                dataType: 'json',
+                success: function(data) {
+                    latitude = data.latitude;
+                    longitude = data.longitude;
+                    submit();
+                }
+            });
         }
     });
 });
--- app/views.py
+++ app/views.py
@@ -1,30 +1,42 @@
-from flask import render_template, flash, redirect, send_from_directory, request
+from flask import render_template, flash, redirect, send_from_directory, request, abort, jsonify
 from app import app, db, user_datastore
 from .forms import LoginForm, NewPostForm
 from .models import User, Post
 from flask.ext.security.decorators import login_required
 from flask.ext.login import current_user
 
-from flask.ext.security.utils import encrypt_password
-import random
+MAX_POSTS_RETURNED = 10
 
-@app.route('/near')
+@app.route('/get_post', methods = ['POST'])
 @login_required
-def near():
-    location = [float(request.args.get('longitude', 0)), float(request.args.get('latitude', 0))]
-    posts = Post.objects(location__near=location)
-    return render_template("near.html", posts=posts)
+def get_post():
+    data = request.get_json()
+    if data is None:
+        abort(404)
+    location = [data['longitude'], data['latitude']]
+    posts = Post.objects(location__near=location).limit(MAX_POSTS_RETURNED)
+    output = []
+    for post in posts:
+        output.append({
+            'id': str(post.id),
+            'author': post.author.user_id,
+            'date_created': post.date_created,
+            'title': post.title,
+            'content': post.content,
+            'view_count': post.view_count,
+            'support_count': post.view_count
+        })
+    return jsonify(posts=output)
+
+@app.route('/add_post', methods = ['POST'])
+@login_required
+def add_post():
+    data = request.get_json()
+    if data is None:
+        abort(404)
+    new_post = Post(author=current_user._get_current_object(), title=data['title'], content=data['content'], location=[data['longitude'], data['latitude']]).save()
+    return jsonify(status="success")
 
 @app.route('/')
 def index():
     return render_template("index.html")
-
-@app.route('/post', methods = ['GET', 'POST'])
-@login_required
-def add_post():
-    form = NewPostForm()
-    if form.validate_on_submit():
-        new_post = Post(author=current_user._get_current_object(), title=form.title.data, content=form.content.data, location=[float(form.longitude.data), float(form.latitude.data)]).save()
-        flash('Post added')
-        return redirect('/')
-    return render_template('add_post.html', form=form)
