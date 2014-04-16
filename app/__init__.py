0e6ddc8ef41d8226ae375b0001db3a752d0fc572
Jonathon Grigg <jonathongrigg@gmail.com>
2014-04-16 15:46:54 +0800

    Work around flask-security dropping password confirmation when account confirmation is enabled

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 from flask.ext.security import Security, MongoEngineUserDatastore
+from flask.ext.security.forms import RegisterForm
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -8,7 +9,7 @@ db = MongoEngine(app)
 
 from .models import User, Role
 user_datastore = MongoEngineUserDatastore(db, User, Role)
-security = Security(app, user_datastore)
+security = Security(app, user_datastore, confirm_register_form=RegisterForm)
 
 from flask_mail import Mail
 mail = Mail(app)
